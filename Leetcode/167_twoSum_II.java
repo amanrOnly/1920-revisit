7e53a6536881ba5c880525f6156041fcf6710fd4
Aman Rana <amanranapoiu@gmail.com>
2021-10-29 20:33:13 +0530

    Create 167_twoSum_II.java

---
+++ Leetcode/167_twoSum_II.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int s=0, e=numbers.length-1;
+        
+        while(s<e){
+            int cSum = numbers[s]+numbers[e];
+            
+            if(cSum==target) return new int[]{s+1, e+1};
+            
+            else if(cSum>target){
+                --e;
+            }
+            
+            else ++s;
+            
+        }
+        
+        return new int[]{-1,-1};
+    }
+}
