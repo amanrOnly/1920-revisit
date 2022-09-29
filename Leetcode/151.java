80a5b70d74a910563d897f11f3be7c8cd487c502
Aman Rana <amanranapoiu@gmail.com>
2022-09-29 22:47:16 +0530

    Update 151.java

--- Leetcode/151.java
+++ Leetcode/151.java
@@ -20,3 +20,28 @@ class Solution {
         return res;
     }
 }
+
+// USING STRING BUILDER- FASTER
+
+class Solution {
+    public String reverseWords(String s) {
+        int i=s.length()-1;
+        StringBuilder res= new StringBuilder("");
+        while(i>=0){
+            while(i>=0 && s.charAt(i)==' ') --i;
+            if(i<0) break;
+            int j=i;
+            
+            while(i>=0 && s.charAt(i)!=' ') --i;
+            if(res.isEmpty()){
+                res=res.append(s.substring(i+1,j+1));
+            }
+            else{
+                res=res.append(" "+s.substring(i+1,j+1));
+            }
+            
+        }
+        String ans=res.toString();
+        return ans;
+    }
+}
