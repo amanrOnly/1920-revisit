01f9f8536c8f7621b323b01f4eba632eb79f7ce6
Aman Rana <amanranapoiu@gmail.com>
2022-11-14 00:26:23 +0530

    Time: 2 ms (36.21%), Space: 45 MB (8.42%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -16,13 +16,14 @@
 class Solution {
     public boolean isBalanced(TreeNode root) {
         if (root == null) { return true; }
-        int left = depth(root.left);
-        int right = depth(root.right);
-        return Math.abs(left - right) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+        int leftHeight=depth(root.left);
+        int rightHeight=depth(root.right);
+        return Math.abs(leftHeight-rightHeight)<=1 && isBalanced(root.left) && isBalanced(root.right);
     }
     
-    private int depth(TreeNode root) {
-        if (root == null) { return 0; }
-        return Math.max(depth(root.left), depth(root.right)) + 1;
+    public int depth(TreeNode root){
+        if(root==null) return 0;
+        return Math.max(depth(root.left), depth(root.right))+1;
     }
+    
 }
