67f7ff9480e1e8b464164435ec5178da1a51d4a3
Aman Rana <amanranapoiu@gmail.com>
2022-11-17 19:35:44 +0530

    Time: 0 ms (100.00%), Space: 50.1 MB (29.46%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -16,7 +16,7 @@
 class Solution {
     public int countNodes(TreeNode root) {
         if(root==null) return 0;
-        else if(root.left==null && root.right==null) return 1;
+        //else if(root.left==null && root.right==null) return 1;
         return countNodes(root.left)+countNodes(root.right)+1;
     }
 }
