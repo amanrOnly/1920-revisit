1069f2b6e45e4c70397cff46baaf0136a7588450
Aman Rana <amanranapoiu@gmail.com>
2022-11-23 23:45:44 +0530

    Time: 347 ms (20.44%), Space: 68.8 MB (24.29%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -9,7 +9,7 @@ public class Solution {
             for (int j = i; j < n; j++) {
                 dp[i][j] = s.charAt(i) == s.charAt(j) && 
                            (j - i < 3 || dp[i + 1][j - 1]);
-                              // corner 
+                        
                 if (dp[i][j] && (res == null || j - i + 1 > res.length())) 
                     res = s.substring(i, j + 1); /* Update result */
             }
